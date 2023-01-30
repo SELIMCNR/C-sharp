378a317d39f1e1587fcf99b1f49e148a54ff63fb
Selim Çınar <90525336+SELIMCNR@users.noreply.github.com>
2023-01-30 20:42:47 +0300

    Add files via upload

---
+++ 07-Classes/Customer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_Classes
+{
+    public class Customer
+    {
+        public string firstName; // field 
+
+
+        // Müşterinin bilgilerini özelliklerini tutar
+        public int Id { 
+            get { return Id; } 
+            set { Id = value; }
+        }
+        // DIŞARDAN DEĞER EKLENDİĞİNDE SET BLOĞU ÇALIŞIR
+        // Dışarıya değer verdiğinde get çalışır.
+        public string FirstName
+        {
+            get { return "Mr"+firstName; } //encapsülation kapsülleme
+            set { firstName = value; }
+        }
+        public string LastName { get; set; }
+        public string City { get; set; }
+
+       
+
+    }
+}
---
+++ 09-Interfaces/Program.cs
@@ -0,0 +1,87 @@
+﻿using _09_Interfaces;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {   //Interfaceler newlenemez abstraclarda newlenemez çünkü soyutlar
+        // Interfacesıntro();
+
+        NewMethod();
+
+        ICustomerDal[] customerDals= new ICustomerDal[3]
+        {
+            new SqlServerCustomerDal(),
+            new OracleServerCustomerDal(),
+            new SqlServerCustomerDal(),
+        };
+        foreach (var customerDal in customerDals)
+        {
+            customerDal.Add();
+        }
+    }
+
+    private static void NewMethod()
+    {
+        CustomerManager customerManager = new CustomerManager();
+        customerManager.Add(new SqlServerCustomerDal());
+    }
+
+    private static void Interfacesıntro()
+    {
+        PersonManager manager = new PersonManager();
+        manager.Add(new Customer { Id = 1, Firstname = "Engin", Lastname = "Demiroğ", Address = "Kahramanmaraş" });
+
+
+        PersonManager manager1 = new PersonManager();
+        Student student = new Student
+        {
+            Id = 1,
+            Firstname = "student",
+            Lastname = "lastname",
+            Department = "Computer",
+        };
+
+        manager1.Add(student);
+    }
+}
+
+interface IPerson
+{
+     int Id { get; set; }
+     string Firstname  { get; set; }
+     string Lastname { get; set; }
+}
+
+class Customer : IPerson  // Iperson ınterfacesinin tüm özelliklerini kullanabilir.
+{
+    public int Id { get ; set ; }
+    public string Firstname { get ; set; }
+    public string Lastname { get ; set; }
+    public string Address { get; set; }
+}
+class Student : IPerson
+{
+    public int Id { get; set; }
+    public string Firstname { get; set; }
+    public string Lastname { get; set; }
+
+    public string Department { get; set; }
+}
+
+class Worker : IPerson
+{
+    public int Id { get; set; }
+    public string Firstname { get; set; }
+    public string Lastname { get; set; }
+
+    public string Department { get; set; }
+}
+
+class PersonManager
+{
+    public void Add(IPerson person) {
+        Console.WriteLine(person.Firstname);
+    }
+
+   
+}
