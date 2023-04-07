68aeed0952a96cfb3170e0b4c795b28cb6b6bd14
Selim Çınar <90525336+SELIMCNR@users.noreply.github.com>
2023-04-07 19:03:57 +0300

    Add files via upload

---
+++ 10_Interfaces_Demo/Program.cs
@@ -0,0 +1,103 @@
+﻿internal class Program
+{
+
+
+    //Solid prensipleri araştır
+    //Interfaceyi iyi kullanma prensibi
+    private static void Main(string[] args)
+    {
+
+        //İnterfacelerden dizi 
+        IWorker[] workers = new IWorker[3]
+        {
+            new Manager(),
+            new Worker(),
+            new Robot(),
+        };
+        //Dizide değerler foreach döngüsünde
+        foreach (var worker in workers)
+        {
+            //Ekrana yazılacak değerler
+            worker.Work();
+        }
+
+        //İnterfaceden dizi
+        IEat[] eats = new IEat[2]
+        {
+            new Worker(),
+            new Manager(),
+         //interfaceyi implement etmedinden new Robot(),
+        };
+
+        foreach (var eat in eats) {
+            eat.Eat();
+        }
+
+
+    
+    }
+
+    //IWorker adlı interface oluşturuldu
+    interface IWorker
+    {
+        void Work();
+    }
+   
+    interface IEat
+    {
+        void Eat();
+    }
+    interface ISalary
+    {
+        void GetSalary();
+    }
+
+    //Classla interface arasında inheritance kalıtım sağlandı
+    class Manager : IWorker, IEat, ISalary
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Yemek yendi ");
+        }
+
+        public void GetSalary()
+        {
+            Console.WriteLine("Para harcandı");
+        }
+
+        public void Work()
+        {
+            Console.WriteLine("İş yapıldı");
+        }
+    }
+
+    //Farklı interfaceler classa kalıtım yoluyla eklendi
+    class Worker : IWorker,IEat, ISalary
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Yemek yendi");
+        }
+
+        public void GetSalary()
+        {
+            Console.WriteLine("Para harcandı");
+        }
+
+        public void Work()
+        {
+            Console.WriteLine("İş yapıldı");
+        }
+    }
+
+    class Robot : IWorker
+    {
+        
+        public void Work()
+        {
+            Console.WriteLine("İş yapıldı");
+        }
+    }
+
+
+}
---
+++ 11_İnheritance_Kalıtım/Program.cs
@@ -0,0 +1,58 @@
+﻿/*
+ Notlar 
+Class 'class' cannot have multiple base classes: 'class_1' and 'class_2' 2 class kalıtıma eklenemez  ama 
+2,3,4 interface kalıtıma eklenebilir.
+
+classların tek başına bir anlamı var 
+İnterfacelerin tek başına bir anlamı yok.
+
+Elinden geldince interface kullan
+ */
+
+//Classdan nesne oluştu
+Customer customer = new Customer();
+//oluşan nesneye değer atandı
+customer.FirstName = "a";
+//oluşan nesne yazdırıldı
+Console.WriteLine(customer.FirstName);
+
+//Classdan 3 indisli dizi oluştu 
+Person[] persons = new Person[3]
+{
+    //dizi içerisine class objeleri ve özellik değerleri eklendi
+    new Customer {FirstName = "Engin"}
+    ,
+    new Student {FirstName = "Derin"} 
+    ,
+    new Person {FirstName = "Salih"}
+
+};
+
+// oluşturulan dizi foreach ile yazdırıldı
+foreach (var person in persons)
+{
+    Console.WriteLine(person.FirstName);
+}
+
+
+class Person
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+}
+
+
+//Customer classına person classında değerler inheritance kalıtım yapıldı
+class Customer : Person
+{
+    //Ekstra özellik eklendi classa
+    public string City { get; set; }
+
+}
+
+class Student : Person
+{
+    public string department { get; set; }
+}
